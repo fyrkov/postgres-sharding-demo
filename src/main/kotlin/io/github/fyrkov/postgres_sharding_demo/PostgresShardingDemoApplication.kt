9f4a8777c46e925309c4f20aecdcb2f670f91dc0
Andrey Chirkov <fyrkov86@gmail.com>
2026-01-14 20:51:56 +0100

    clean up config adjustments

--- src/main/kotlin/io/github/fyrkov/postgres_sharding_demo/PostgresShardingDemoApplication.kt
+++ src/main/kotlin/io/github/fyrkov/postgres_sharding_demo/PostgresShardingDemoApplication.kt
@@ -1,8 +1,6 @@
 package io.github.fyrkov.postgres_sharding_demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
 import org.springframework.boot.jooq.autoconfigure.JooqAutoConfiguration
 import org.springframework.boot.runApplication
 
